a43e11e5904a2fc9e37fa66461c4c111422db7d0
iviWebStudio <mmartin1@rambler.ru>
2021-08-28 14:05:51 +0400

    added api index file

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+/**
+ * API's main exporter.
+ *
+ * This is simple intermediate place for more readable imports.
+ * @since 0.1.0
+ */
+import {fetchAllCategories, fetchImageByID, fetchImages} from "./api";
+
+export {fetchAllCategories, fetchImages, fetchImageByID}
